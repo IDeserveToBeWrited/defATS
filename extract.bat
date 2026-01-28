3c4ecb6a1c32654181ed4227ce10a43eec84a2e3
IDeserveToBeWrited <dodo.coowbooy@gmail.com>
2026-01-28 11:30:37 +0100

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -8,27 +8,28 @@ SET LOCATION=%userprofile%\Documents\defATS
 SET /A COUNT=0
 SET /A CURRENT=1
 
+:: Create location
+if not exist %LOCATION% MKDIR %LOCATION%
+
 :: Clean any remaining files
 echo Cleaning the directory
 del /f/s/q %LOCATION% > nul
 rmdir /s/q %LOCATION%
 
-:: Create location
-if not exist %LOCATION% MKDIR %LOCATION%
-
-:: Location to SCS Extractor
-SET EXTRACTOR="C:\Program Files (x86)\Steam\steamapps\common\Euro Truck Simulator 2\scs_extractor.exe"
-:: Get the SCS Game Archive Extractor here: http://download.eurotrucksimulator2.com/scs_extractor.zip
+:: Location to sk-zk extractor
+SET EXTRACTOR="C:\Users\doode\Downloads\extractor-2025-12-11-win-x64\extractor-2025-12-11-win-x64\extractor.exe"
+:: Get the sk-zk extractor here: https://github.com/sk-zk/Extractor/releases
 
 :: Set 7z.exe location for archiving
 SET ZIP="C:\Program Files\7-Zip\7z.exe"
 
-:: Count Archives
-for %%l in (*.scs) DO (IF NOT %%l == base.scs IF NOT %%l == base_cfg.scs IF NOT %%l == core.scs IF NOT %%l == effect.scs IF NOT %%l == locale.scs SET /A COUNT=COUNT+1 )
-
-:: Extract and clean archives
-for %%a in (*.scs) DO (IF NOT %%a == base.scs IF NOT %%a == base_cfg.scs IF NOT %%a == core.scs IF NOT %%a == effect.scs IF NOT %%a == locale.scs echo|set /p="^(!CURRENT!\%COUNT%^) Unpacking %%~na" & %EXTRACTOR% %%a %LOCATION%\%%~na >NUL & echo|set /p=", Clean-up" & (for /D %%c in (%LOCATION%\%%~na\*) do ((IF NOT %%~nc == def RMDIR "%%c" /s /q >nul))) & DEL %LOCATION%\%%~na\%%~na.manifest.sii >nul & echo . Done. & SET /A CURRENT+=1 )
+:: Extract archives
+%EXTRACTOR% -p=/def -S -q -a "C:\Program Files (x86)\Steam\steamapps\common\American Truck Simulator" -d %LOCATION%
+%EXTRACTOR% -S "C:\Program Files (x86)\Steam\steamapps\common\American Truck Simulator\version.scs" -d %LOCATION% 
 
 ECHO Zipping archive
 %ZIP% a -mx=9 %LOCATION%\def %LOCATION%\*
 ECHO Zipped
+
+ECHO DONE LOL
+pause
